9c6d9254af2a43ace8c4f3a72006460e3a4d6ddf
justintwoo <81633300+justintwoo@users.noreply.github.com>
2022-02-10 14:38:24 -0500

    Progress

--- main.js
+++ main.js
@@ -10,5 +10,17 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    console.log("Button 1 was clicked")
+
+    let RandNum = Math.random() * 100;
+    let randomNumDiv = document.getElementById('random_num');
+    randomNumDiv.innerHTML = RandNum
+   
+function button2Clicked() {
+    console.log("Button 2 was clicked")
+
+    let color_change = document.getElementById("change_color").bgcolor="#Insert Color Here";
+
 }
+}
+
